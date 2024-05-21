6104fb17ac29b3645ca5f4deef72b144b02f8586
scrawach <scrawach@gmail.com>
2024-05-21 17:49:20 +0300

    added effective distance for bullet, after it enable gravity for bullets

--- client/PiratesUnderAttacks/Assets/CodeBase/Gameplay/Bullet.cs
+++ client/PiratesUnderAttacks/Assets/CodeBase/Gameplay/Bullet.cs
@@ -5,8 +5,21 @@ namespace CodeBase.Gameplay
     public class Bullet : MonoBehaviour
     {
         [SerializeField] private float _speed;
+        [SerializeField] private float _effectiveDistance;
+        [SerializeField] private float _gravityDamp;
 
-        private void Update() => 
-            transform.Translate(transform.forward * Time.deltaTime * _speed, Space.World);
+        private float _elapsedDistance;
+        
+        private void Update()
+        {
+            var moveStep = Time.deltaTime * _speed;
+            var movement = transform.forward * moveStep;
+
+            if (_elapsedDistance >= _effectiveDistance)
+                movement += (Physics.gravity * Time.deltaTime) / _gravityDamp;
+            
+            transform.Translate(movement, Space.World);
+            _elapsedDistance += moveStep;
+        }
     }
 }
