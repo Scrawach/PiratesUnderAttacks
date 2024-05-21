c637449d90ff8caa4f7a202de90a154017e36794
scrawach <scrawach@gmail.com>
2024-05-21 12:21:00 +0300

    added game bootstrapper, renamed sample scene to bootstrap, added game bootstrapper on project context by Reflex

---
+++ client/PiratesUnderAttacks/Assets/CodeBase/Infrastructure/GameBootstrapper.cs
@@ -0,0 +1,13 @@
+using Reflex.Core;
+using UnityEngine;
+
+namespace CodeBase.Infrastructure
+{
+    public class GameBootstrapper : MonoBehaviour, IInstaller
+    {
+        public void InstallBindings(ContainerBuilder containerBuilder)
+        {
+            
+        }
+    }
+}
