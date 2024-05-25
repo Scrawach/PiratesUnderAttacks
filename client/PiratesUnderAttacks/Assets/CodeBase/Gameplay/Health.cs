d2d9ca8bc7b0461fe932e7b3f4df608995b9b069
scrawach <scrawach@gmail.com>
2024-05-25 16:36:05 +0300

    implemented health component

---
+++ client/PiratesUnderAttacks/Assets/CodeBase/Gameplay/Health.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace CodeBase.Gameplay
+{
+    public class Health : MonoBehaviour
+    {
+        [field: SerializeField] public int Max { get; private set; }
+        [field: SerializeField] public int Current { get; private set; }
+        
+        public event Action Changed;
+
+        public float Ratio => Current * 1f / Max;
+        
+        public void TakeDamage(int damage)
+        {
+            Current = Mathf.Clamp(Current - damage, 0, Max);
+            Changed?.Invoke();
+        }
+    }
+}
