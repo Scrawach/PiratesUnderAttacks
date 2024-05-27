487bc433eab9264aa01716f7b421069d478a216b
scrawach <scrawach@gmail.com>
2024-05-27 15:16:01 +0300

    implemented damper for ship movement

--- client/PiratesUnderAttacks/Assets/CodeBase/Gameplay/Ship.cs
+++ client/PiratesUnderAttacks/Assets/CodeBase/Gameplay/Ship.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace CodeBase.Gameplay
@@ -16,25 +15,29 @@ namespace CodeBase.Gameplay
         
         public bool IsMoving { get; private set; }
 
+        private float _strength;
+        private float _currentStrength;
+
         public void LookAt(Vector3 target)
         {
             var direction = target - transform.position;
-
+            
             if (direction != Vector3.zero)
             {
                 IsMoving = true;
                 _targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+                _strength = 1;
             }
             else
             {
                 IsMoving = false;
+                _strength = 0;
             }
         }
         
         private void Update()
         {
-            if (IsMoving)
-                ProcessMovement();
+            ProcessMovement();
             
             ProcessRotation();
         }
@@ -42,7 +45,9 @@ namespace CodeBase.Gameplay
         private void ProcessMovement()
         {
             var timeStep = Time.deltaTime * _speed;
-            Movement = transform.forward * timeStep;
+            _currentStrength += (_strength - _currentStrength) * Time.deltaTime;
+            
+            Movement = transform.forward * timeStep * _currentStrength;
             transform.Translate(Movement, Space.World);
         }
 
