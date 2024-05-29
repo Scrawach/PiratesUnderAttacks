98e7a5257eff0ef8c1d24c74482d0ff6c5913239
scrawach <scrawach@gmail.com>
2024-05-29 13:20:31 +0300

    generate new network schemas

--- client/PiratesUnderAttacks/Assets/CodeBase/Network/Generated/GameRoomState.cs
+++ client/PiratesUnderAttacks/Assets/CodeBase/Network/Generated/GameRoomState.cs
@@ -10,28 +10,28 @@ using Action = System.Action;
 
 namespace CodeBase.Generated {
 	public partial class GameRoomState : Schema {
-		[Type(0, "string")]
-		public string mySynchronizedProperty = default(string);
+		[Type(0, "map", typeof(MapSchema<PlayerSchema>))]
+		public MapSchema<PlayerSchema> players = new MapSchema<PlayerSchema>();
 
 		/*
 		 * Support for individual property change callbacks below...
 		 */
 
-		protected event PropertyChangeHandler<string> __mySynchronizedPropertyChange;
-		public Action OnMySynchronizedPropertyChange(PropertyChangeHandler<string> __handler, bool __immediate = true) {
+		protected event PropertyChangeHandler<MapSchema<PlayerSchema>> __playersChange;
+		public Action OnPlayersChange(PropertyChangeHandler<MapSchema<PlayerSchema>> __handler, bool __immediate = true) {
 			if (__callbacks == null) { __callbacks = new SchemaCallbacks(); }
-			__callbacks.AddPropertyCallback(nameof(this.mySynchronizedProperty));
-			__mySynchronizedPropertyChange += __handler;
-			if (__immediate && this.mySynchronizedProperty != default(string)) { __handler(this.mySynchronizedProperty, default(string)); }
+			__callbacks.AddPropertyCallback(nameof(this.players));
+			__playersChange += __handler;
+			if (__immediate && this.players != null) { __handler(this.players, null); }
 			return () => {
-				__callbacks.RemovePropertyCallback(nameof(mySynchronizedProperty));
-				__mySynchronizedPropertyChange -= __handler;
+				__callbacks.RemovePropertyCallback(nameof(players));
+				__playersChange -= __handler;
 			};
 		}
 
 		protected override void TriggerFieldChange(DataChange change) {
 			switch (change.Field) {
-				case nameof(mySynchronizedProperty): __mySynchronizedPropertyChange?.Invoke((string) change.Value, (string) change.PreviousValue); break;
+				case nameof(players): __playersChange?.Invoke((MapSchema<PlayerSchema>) change.Value, (MapSchema<PlayerSchema>) change.PreviousValue); break;
 				default: break;
 			}
 		}
---
+++ client/PiratesUnderAttacks/Assets/CodeBase/Network/Generated/PlayerSchema.cs
@@ -0,0 +1,87 @@
+// 
+// THIS FILE HAS BEEN GENERATED AUTOMATICALLY
+// DO NOT CHANGE IT MANUALLY UNLESS YOU KNOW WHAT YOU'RE DOING
+// 
+// GENERATED USING @colyseus/schema 2.0.32
+// 
+
+using Colyseus.Schema;
+using Action = System.Action;
+
+namespace CodeBase.Generated {
+	public partial class PlayerSchema : Schema {
+		[Type(0, "string")]
+		public string username = default(string);
+
+		[Type(1, "ref", typeof(Vector2Schema))]
+		public Vector2Schema position = new Vector2Schema();
+
+		[Type(2, "uint8")]
+		public byte skinId = default(byte);
+
+		[Type(3, "uint16")]
+		public ushort score = default(ushort);
+
+		/*
+		 * Support for individual property change callbacks below...
+		 */
+
+		protected event PropertyChangeHandler<string> __usernameChange;
+		public Action OnUsernameChange(PropertyChangeHandler<string> __handler, bool __immediate = true) {
+			if (__callbacks == null) { __callbacks = new SchemaCallbacks(); }
+			__callbacks.AddPropertyCallback(nameof(this.username));
+			__usernameChange += __handler;
+			if (__immediate && this.username != default(string)) { __handler(this.username, default(string)); }
+			return () => {
+				__callbacks.RemovePropertyCallback(nameof(username));
+				__usernameChange -= __handler;
+			};
+		}
+
+		protected event PropertyChangeHandler<Vector2Schema> __positionChange;
+		public Action OnPositionChange(PropertyChangeHandler<Vector2Schema> __handler, bool __immediate = true) {
+			if (__callbacks == null) { __callbacks = new SchemaCallbacks(); }
+			__callbacks.AddPropertyCallback(nameof(this.position));
+			__positionChange += __handler;
+			if (__immediate && this.position != null) { __handler(this.position, null); }
+			return () => {
+				__callbacks.RemovePropertyCallback(nameof(position));
+				__positionChange -= __handler;
+			};
+		}
+
+		protected event PropertyChangeHandler<byte> __skinIdChange;
+		public Action OnSkinIdChange(PropertyChangeHandler<byte> __handler, bool __immediate = true) {
+			if (__callbacks == null) { __callbacks = new SchemaCallbacks(); }
+			__callbacks.AddPropertyCallback(nameof(this.skinId));
+			__skinIdChange += __handler;
+			if (__immediate && this.skinId != default(byte)) { __handler(this.skinId, default(byte)); }
+			return () => {
+				__callbacks.RemovePropertyCallback(nameof(skinId));
+				__skinIdChange -= __handler;
+			};
+		}
+
+		protected event PropertyChangeHandler<ushort> __scoreChange;
+		public Action OnScoreChange(PropertyChangeHandler<ushort> __handler, bool __immediate = true) {
+			if (__callbacks == null) { __callbacks = new SchemaCallbacks(); }
+			__callbacks.AddPropertyCallback(nameof(this.score));
+			__scoreChange += __handler;
+			if (__immediate && this.score != default(ushort)) { __handler(this.score, default(ushort)); }
+			return () => {
+				__callbacks.RemovePropertyCallback(nameof(score));
+				__scoreChange -= __handler;
+			};
+		}
+
+		protected override void TriggerFieldChange(DataChange change) {
+			switch (change.Field) {
+				case nameof(username): __usernameChange?.Invoke((string) change.Value, (string) change.PreviousValue); break;
+				case nameof(position): __positionChange?.Invoke((Vector2Schema) change.Value, (Vector2Schema) change.PreviousValue); break;
+				case nameof(skinId): __skinIdChange?.Invoke((byte) change.Value, (byte) change.PreviousValue); break;
+				case nameof(score): __scoreChange?.Invoke((ushort) change.Value, (ushort) change.PreviousValue); break;
+				default: break;
+			}
+		}
+	}
+}
